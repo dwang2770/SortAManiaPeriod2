00db34657042fd9e85dec8f4080db6fd3b089bfe
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-02-28 09:23:58 -0500

    dsdsd

---
+++ Sortamaniamain/src/Runner.java
@@ -0,0 +1,25 @@
+
+public class Runner {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		long start = System.nanoTime();
+		TeAm4.bubbleSort(test1);
+		long end = System.nanoTime();
+		long time = end - start;
+		System.out.println("Test1 took: " + time + " nanoseconds");
+	}
+	
+	public static int [] randArr(int length, int high)
+	{
+		int [] randoArr = new int [length];
+		for ( int i = 0; i < length; i++)
+		{
+			int j = (int) (Math.random() * high);
+			randoArr[i] = j; 
+		}
+		return randoArr;
+	}
+}
--- Sortamaniamain/src/TeAm4.java
+++ Sortamaniamain/src/TeAm4.java
@@ -23,7 +23,7 @@ public class TeAm4 extends SortCompetition
 	
 	public int challengeOne(int[] arr)
 	{
-		Arrays.sort(arr);
+		bubbleSort(arr);
 		return (arr[arr.length/2]+arr[arr.length/2+1])/2;
 	}
 	
@@ -50,5 +50,42 @@ public class TeAm4 extends SortCompetition
 	public String greeting() 
 	{
 		return "xd";
+	}
+	
+	public static void bubbleSort( int [] list1)
+	{
+		int i = 0;
+		int swap = 0;
+		
+		while (i<list1.length-1)
+		{
+			if ( list1[i] > list1[i + 1])
+			{
+				swapInt(list1, i, i+1);
+				//counts the amount of time you had to swap the array
+				swap ++;
+				// after the swap we move on to the next position
+				i++;
+			}
+
+			else
+			{
+				i++;
+			}
+			
+		}
+
+		if(swap != 0)
+		{
+		bubbleSort(list1);
+		}
+	
+	}
+	
+	public static void swapInt(int[] arr, int index1, int index2)
+	{
+		int x = index1;
+		arr[index1] = arr[index2];
+		arr[index2] = x;
 	}
 }
