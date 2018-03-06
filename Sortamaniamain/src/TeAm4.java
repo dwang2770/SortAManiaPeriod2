2c7b59e26ee92747f6b4593ef09c47cbccec6c20
BT_1N3_23 <BT_1N3_23@BT_1N3_23.bktech.local>
2018-03-06 09:15:28 -0500

    lawl it works xd lul u suk

--- Sortamaniamain/src/TeAm4.java
+++ Sortamaniamain/src/TeAm4.java
@@ -48,7 +48,7 @@ public class TeAm4 extends SortCompetition
 	
 	public int challengeThree(int[] arr)
 	{
-		bubbleSort(arr);
+		quickSort(arr,0,arr.length);
 		if (arr.length%2 != 0)
 		{
 			int x = arr.length/2;
@@ -151,4 +151,51 @@ public class TeAm4 extends SortCompetition
 	         StringQuickSort(arr, pivot+1, high);
 	     }
 	 }
+
+
+	public static void quickSort(int arr[], int low, int high)
+		{
+		    if (low < high)
+		    {
+		        int pivot = partitionInt(arr, low, high);
+		         
+		        quickSort(arr, low, pivot-1);
+		        quickSort(arr, pivot+1, high);
+		    }
+		}
+	public static int partitionInt(int [] list, int front, int back)
+    {
+        //pivot is the last number
+		int pivot = list[back]; 
+		// index of smaller element
+		int i = (front - 1); 
+        //for loop that goes through the whole array
+        for (int j=front; j<back; j++)
+        {
+            /* If list[j] is smaller than or equal to pivot swap arr[i] and arr[j]
+               example arry nums = {1,5,2,3}
+               
+               in this case j will be 1 and i will be -1
+               since 1 is less than 3 we will swap 1 with int at position i(0; which is int 1)
+               
+               after we check that 5 is greater than 3 and we do nothing
+               we then check that 2 is less that 3, at this moment i is now 1(0 + 1) and we will swap 2 with position 1(int 5)
+               
+               after this we are at int 3 which is the last number in the array and ends the for loop
+            */
+            if (list[j] <= pivot)
+            {
+                i++;
+               swapInt(list,i,j);
+            }
+        }
+        /*
+         * after all that sorting we are guaranteed that all the ints smaller than the pivot is before position i and i itself
+         * so at the very end we will swap the pivot(arr[back]) with the int at position i +1
+         */
+      swapInt(list,i+1,back );
+       //returns i+1 because i is always 1 less than the index due to front -1
+        return i+1;
+    }
+	
 }
